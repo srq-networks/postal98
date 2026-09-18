761ddf53d103db27669462a8e6b0b92e13588a6b
Daniel Poprawski <danielbpoprawski@gmail.com>
2026-09-18 12:51:52 -0400

    Updated Something Unique section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ export function Home() {
         video={rawUrl(uploads.heroVideo)}
         padding={['9vw', '5vw']}
       >
-        <div className="row row-4col !pt-[27px] !pb-0 min-h-[229.4px]">
+        <div className="row row-4col pt-[27px]! pb-0! min-h-[229.4px]">
           <div className="col col-1_4 col-empty" />
           <div className="col col-1_4 text-center">
             <Button href={rawUrl(uploads.menuPdf)} external>
@@ -42,17 +42,19 @@ export function Home() {
       </Hero>
 
       {/* Something Unique */}
-      <section className="section bg-white !py-0 !pb-[1px] min-h-[828px]">
-        <div className="row row-full !pt-[27px] !pb-[27px]">
-          <div className="col col-1_2 pt-[6vw] pr-[2vw] pb-[6vw] pl-[10vw] max-lg:px-[10vw]">
+      <section className="section bg-[#fdfdfd] py-0! pb-px!">
+        {/* Container  */}
+        <div className="flex flex-row p-4 pt-24 mx-auto justify-around max-lg:flex-col">
+          <div className="flex-col justify-left">
             {/* Divi hides this module's content on tablet and phone */}
-            <div className="txt mod -mt-[50px] !mb-[20px] max-w-[600px] font-cairo font-semibold text-[18px] leading-[1.8em] max-lg:hidden">
+            <div className="txt mod mt-[-50px] mb-[20px]! max-w-[600px] font-cairo font-semibold text-[18px] leading-[1.8em] max-lg:text-center">
               <h4 className="font-cairo font-bold uppercase text-brand-red tracking-[4px] leading-[1.6em]">
                 Est. 2020
               </h4>
-              <h1 className="font-outside text-[80px]">Something Unique</h1>
+              {/* <h1 className="font-outside text-[80px]">Something Unique</h1> */}
+              <Stripe title="Something Unique" className="py-0! mt-[8px]! mx-auto" />
             </div>
-            <div className="txt mod !pb-0 max-w-[600px] font-cairo font-semibold text-[16px] leading-[2em]">
+            <div className="txt mod pb-0! max-w-[600px] font-cairo font-semibold text-[16px] leading-[2em]">
               <h3 className="text-justify">
                 At Postal 98 Cafe, we specialize in freshly ground Guatemalan coffee. Enjoy a savory
                 breakfast wrap or sandwich along with a steaming hot coffee is that is both rich and
@@ -69,28 +71,24 @@ export function Home() {
                 Come out and see us, we are ready to serve you with a smile!
               </h3>
             </div>
-            <div className="mod !mt-[18px]">
+            <div className="my-[18px]! mx-auto max-lg:text-center">
               <Button href="#info_direction">Visit Us</Button>
             </div>
           </div>
-          <div className="col col-1_2">
-            <div className="mod pr-[2px] max-lg:text-center">
-              <img
-                src={fullUrl(uploads.somethingUnique)}
-                alt=""
-                className="w-full"
-                width={1446}
-                height={1446}
-              />
-            </div>
+          <div className="max-w-[400px]">
+            <img
+              src={fullUrl(uploads.somethingUnique)}
+              alt=""
+              className=" inset-0 h-full w-full object-cover max-lg:static max-lg:h-auto"
+            />
           </div>
         </div>
-        <Stripe title="Specialties" className="!py-0 !mt-[8px]" />
       </section>
 
       {/* House Specialties */}
-      <section className="section chalkboard !pt-[1px] !pb-[64px]">
-        <div className="row !w-[90%] !max-w-[1180px] !pt-[40px] !pb-0">
+      <Stripe title="Specialties" className="py-0! mt-[8px]!" />
+      <section className="section chalkboard pt-px! pb-[64px]!">
+        <div className="row w-[90%]! max-w-[1180px]! pt-[40px]! pb-0!">
           <div className="txt mx-auto mb-[32px] max-w-[640px] text-center font-cairo font-semibold text-[16px] leading-[1.8em] text-white/70">
             <h4 className="font-cairo font-bold uppercase text-brand-yellow tracking-[4px] leading-[1.6em] text-[14px]">
               House Favorites
@@ -116,9 +114,9 @@ export function Home() {
       <WeeklySpecials variant="home" />
 
       {/* Other Items */}
-      <section className="section bg-white !py-0 !mt-[5px] min-h-[166px]">
-        <Stripe title="Other Items" className="!py-[28px]" />
-        <div className="row !pt-0 !pb-[27px]">
+      <section className="section bg-white py-0! mt-[5px]! min-h-[166px]">
+        <Stripe title="Other Items" className="py-[28px]" />
+        <div className="row pt-0! pb-[27px]!">
           <div className="col col-4_4">
             <div className="txt mod mx-auto max-w-[600px] text-center font-cairo font-semibold text-[18px] leading-[1.8em]">
               <p>Coffee – Desserts – Specialty Drinks – And Much More</p>
@@ -126,7 +124,7 @@ export function Home() {
           </div>
         </div>
       </section>
-      <section className="section bg-black !pt-[2px] !pb-[5px]">
+      <section className="section bg-black pt-[2px]! pb-[5px]!">
         <div className="row">
           <div className="col col-4_4">
             <div className="mod">
